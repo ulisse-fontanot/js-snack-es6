e1e90dc0b2b7ceab43aebe016f4699a5755e9220
ulisse-fontanot <ulissef99@gmail.com>
2021-01-19 17:51:45 +0100

    corretti tutti es

--- es-2/js/script.js
+++ es-2/js/script.js
@@ -1,12 +1,12 @@
 const array = [];
 
-for (var i = 1; i <= 10; i++) {
+for (let i = 1; i <= 10; i++) {
   array.push(i)
 }
 
 console.log(array);
 
-for (var i = 0; i < array.length; i++) {
+for (let i = 0; i < array.length; i++) {
   if (array[i]%2 == 0) {
     document.getElementById('green').innerHTML +=array[i];
   } else {
